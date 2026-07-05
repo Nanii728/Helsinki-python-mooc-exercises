b9145e4d5695c4cc61525c26e0a3ef7ea4c1058a
NANII <hanicho.728@gmail.com>
2026-07-05 16:17:18 +0300

    Hello visual studio code done

--- part04-01_hello_visual_studio_code/src/hello_visual_studio_code.py
+++ part04-01_hello_visual_studio_code/src/hello_visual_studio_code.py
@@ -1,1 +1,10 @@
 # Write your solution here
+while True:
+    choice=input("Editor:").lower()
+    if choice=="visual studio code":
+        print("an excellent choice!")
+        break
+    elif choice in ["word","note pad"]:
+        print("awful")
+    else:
+        print("not good")
