8a83aeddabc8717136c8f5285efebb5cfc09a81f
NANII <hanicho.728@gmail.com>
2026-07-09 13:20:00 +0300

    shape

--- part04-07_shape/src/shape.py
+++ part04-07_shape/src/shape.py
@@ -1,4 +1,18 @@
 # Copy here code of line function from previous exercise and use it in your solution
+def line(num,char):
+    if len(char)>0:
+        print(char[0]*num)
+    else:
+        print("*" * num)
+
 # You can test your function by calling it within the following block
+
+def shape(width,tr_char,height,rec_char):
+    for i in range(1,width+1):
+        line(i, tr_char)
+    
+    for i in range(height):
+        line(width, rec_char)
+
 if __name__ == "__main__":
-    shape(5, "x", 2, "o")
+    shape(5, "x", 3, "*")
