21a8dda810dff690a33fb771398964666718828e
NANII <hanicho.728@gmail.com>
2026-07-09 13:10:13 +0300

    square

--- part04-05_square/src/square.py
+++ part04-05_square/src/square.py
@@ -1,8 +1,17 @@
 # Copy here code of line function from previous exercise
+def line(num,char):
+    if len(char)>0:
+        print(char[0]*num)
+    else:
+        print("*" * num)
+
 
 def square(size, character):
     # You should call function line here with proper parameters
-    line(4, "x")
+    for i in range(size):
+        line(size, character)
+
+    
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
