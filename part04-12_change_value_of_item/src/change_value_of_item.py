cb827dd509cb7d4c9f141d8cb70843c9ac702956
NANII <hanicho.728@gmail.com>
2026-07-10 17:06:37 +0300

    List introduction

--- part04-12_change_value_of_item/src/change_value_of_item.py
+++ part04-12_change_value_of_item/src/change_value_of_item.py
@@ -1,1 +1,9 @@
 # Write your solution here
+holder=[1,2,3,4,5]
+while True:
+    index=int(input("Index:"))
+    if index==-1:
+        break
+    new=int(input("New value"))
+    holder[index]=new
+    print(holder)
