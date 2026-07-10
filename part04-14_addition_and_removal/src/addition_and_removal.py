4f96068b115f4b3e530ad8f8dd353a04eee5b2fe
NANII <hanicho.728@gmail.com>
2026-07-10 17:31:45 +0300

    Pop and Append methods of list

--- part04-14_addition_and_removal/src/addition_and_removal.py
+++ part04-14_addition_and_removal/src/addition_and_removal.py
@@ -1,1 +1,18 @@
 # Write your solution here
+holder=[]
+print("The list is now []")
+while True:
+    choice=input("a(d)d, (r)emove or e(x)it:")
+    if choice=='d':
+        if len(holder)==0:
+            holder.append(1)
+        else:
+            holder.append(holder[-1] + 1)
+    elif choice=='r':
+        holder.pop()
+    elif choice=='x':
+        print("Bye!")
+        break
+    print(f"The list is now {holder}")
+
+
