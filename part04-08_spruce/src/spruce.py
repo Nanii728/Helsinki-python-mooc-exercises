64aba45eda5176fb711e9b37244fdf447c73bd76
NANII <hanicho.728@gmail.com>
2026-07-10 16:09:28 +0300

    Spruce

--- part04-08_spruce/src/spruce.py
+++ part04-08_spruce/src/spruce.py
@@ -1,4 +1,10 @@
 # Write your solution here
+def spruce(num):
+    print("a spruce!")
+    for i in range(num):
+        print(f"{" " *(num-i-1)}{"*"*(2*i + 1)}")
+    print(f"{" " * (num-1)}*")
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
-    spruce(5)
+    spruce(3)
