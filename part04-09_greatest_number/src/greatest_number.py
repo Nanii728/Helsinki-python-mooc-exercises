36375644db74da0315009efbd6e366c7a322b147
NANII <hanicho.728@gmail.com>
2026-07-10 16:43:57 +0300

    Return value in function

--- part04-09_greatest_number/src/greatest_number.py
+++ part04-09_greatest_number/src/greatest_number.py
@@ -1,4 +1,6 @@
 # Write your solution here
+def greatest_number(num1, num2, num3):
+    return max(num1, num2, num3)
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     greatest = greatest_number(5, 4, 8)
--- part04-10_same_characters/src/same_characters.py
+++ part04-10_same_characters/src/same_characters.py
@@ -1,4 +1,11 @@
 # Write your solution here
+def same_chars(word,ind1,ind2):
+    if (ind2 < len(word))and (ind1 < len(word)):
+        return word[ind1] == word[ind2]
+    else:
+        return False
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     print(same_chars("coder", 1, 2))
+    print(same_chars("abracadabra", 0, 3))
