42a89df5c7e399f4e382be44c374d792151a2e29
NANII <hanicho.728@gmail.com>
2026-07-11 14:52:48 +0300

    All the longest strings

--- part04-31_all_longest_in_list/src/all_longest_in_list.py
+++ part04-31_all_longest_in_list/src/all_longest_in_list.py
@@ -1,1 +1,13 @@
 # Write your solution here
+def all_the_longest(items):
+    long=""
+    list_of_long=[]
+    for i in items:
+        if len(i)>len(long):
+            long=i
+
+    for i in items:
+        if len(long)==len(i):
+            list_of_long.append(i)
+
+    return(list_of_long)
