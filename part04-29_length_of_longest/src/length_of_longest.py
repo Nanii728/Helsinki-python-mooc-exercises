1f46bad24f54b175d0fc48d2a952e75e48db2c34
NANII <hanicho.728@gmail.com>
2026-07-11 14:46:37 +0300

    Finding the best or the worst item in a list

--- part04-29_length_of_longest/src/length_of_longest.py
+++ part04-29_length_of_longest/src/length_of_longest.py
@@ -1,1 +1,7 @@
 # Write your solution here
+def length_of_longest(list):
+    max_length=0
+    for i in list:
+        if len(i)>max_length:
+            max_length=len(i)
+    return max_length
--- part04-30_shortest_in_list/src/shortest_in_list.py
+++ part04-30_shortest_in_list/src/shortest_in_list.py
@@ -1,1 +1,12 @@
 # Write your solution here
+def shortest(items):
+    min_length=len(items[0])
+    short=""
+    for i in items:
+        if len(i)<min_length:
+            min_length=len(i)
+            short=i
+    return short
+
+if __name__=="__main__":
+    print(shortest(["abc", "ab"]))
