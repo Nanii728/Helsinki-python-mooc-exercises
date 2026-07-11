66932195f419737335098b57be95c229fe2f9926
NANII <hanicho.728@gmail.com>
2026-07-11 13:42:45 +0300

    Additional exercise on list

--- part04-27_sum_of_lists/src/sum_of_lists.py
+++ part04-27_sum_of_lists/src/sum_of_lists.py
@@ -1,1 +1,9 @@
 # Write your solution here
+def list_sum(list1,list2):
+    sum_by_index=[]
+    for i in range(len(list1)):
+        sum_by_index.append(list1[i]+list2[i])
+    return sum_by_index
+
+
+        
--- part04-28_distinct_numbers/src/distinct_numbers.py
+++ part04-28_distinct_numbers/src/distinct_numbers.py
@@ -1,1 +1,9 @@
 # Write your solution here
+def distinct_numbers(list):
+    distinct=[]
+    for i in list:
+        if i not in distinct:
+            distinct.append(i)
+    distinct.sort()
+    return distinct
+
