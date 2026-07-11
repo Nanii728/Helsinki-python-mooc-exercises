22f32dc4eb11f0e36e3edaae4ee3ecacbc7574ce
NANII <hanicho.728@gmail.com>
2026-07-11 13:09:11 +0300

    Anagram and List of stars

--- part04-23_anagrams/src/anagrams.py
+++ part04-23_anagrams/src/anagrams.py
@@ -1,1 +1,10 @@
 # Write your solution here
+def anagrams(x:str,y:str)->bool:
+    if sorted(x)==sorted(y):
+        return True
+    else:
+        return False
+
+if __name__=="__main__":
+    print(anagrams("hana","text"))
+    print(anagrams("pii","ip"))
