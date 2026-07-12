706db9d9a877fd85f06f0594af82411f0bb41d46
NANII <hanicho.728@gmail.com>
2026-07-12 14:31:27 +0300

    No vowel-No shouting EX join  and isupper method

--- part04-35_no_vowels_allowed/src/no_vowels_allowed.py
+++ part04-35_no_vowels_allowed/src/no_vowels_allowed.py
@@ -1,1 +1,14 @@
 # Write your solution here
+def no_vowels(text):
+    items=text.split()
+    result=[]
+    for k in items:
+        for i in ["a","e","i","o","u"]:
+            while i in k:
+                k=k.replace(i,"")
+        result.append(k)
+        
+    return " ".join(result)
+
+if __name__=="__main__":
+    print(no_vowels("this is an example"))
--- part04-36_no_shouting_allowed/src/no_shouting_allowed.py
+++ part04-36_no_shouting_allowed/src/no_shouting_allowed.py
@@ -1,1 +1,9 @@
 # Write your solution here
+def no_shouting(items):
+    pruned_list=[]
+    for i in items:
+        if i.isupper():
+            continue
+        else:
+            pruned_list.append(i)
+    return pruned_list
